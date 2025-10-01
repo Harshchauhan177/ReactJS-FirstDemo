5f0f6631052db4aaefa25a11491b2061c094bc71
Harshchauhan177 <harsh7617rajput@gmail.com>
2025-10-01 17:40:51 +0530

    fetch data Using API is DONE.
    in this we use the asyns and await.

--- beta-c/src/pages/Home.js
+++ beta-c/src/pages/Home.js
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import mainStyles from "../assets/css/Main.module.css";
 import Navbar from "../components/layouts/Navbar";
 import Footer from "../components/layouts/Footer";
 
 function Home() {
+  // const url = "https://randomuser.me/api/";
+  // const apiRequest = async () => {
+  //   const fetchData = await fetch(url, { method: "GET" });
+  //   const data = await fetchData.json();
+  //   console.log(data);
+  // };
+  // apiRequest();
+
+  const url = process.env.REACT_APP_API_URL;
+  const [data, setData] = useState("");
+  const apiRequest = async () => {
+    await fetch(url, { method: "GET" }).then(async (response) => {
+      response = await response.json();
+      return setData(response.results[0]);
+    });
+    // console.log(data);
+  };
+  console.log(data);
+  useEffect(() => {
+    apiRequest();
+  }, []);
+
   return (
     <>
       <Navbar />
       <div style={{ minHight: "80vh" }}>
         <div className={mainStyles.homePage}>
-          <h1>Welcome to My website</h1>
           <p>
-            This is a simple React application using Vite as the build tool.
+            hi my name is {data.name?.title} {data.name?.first}{" "}
+            {data.name?.last}
           </p>
+          <img src={data.picture?.large} alt="hi" />
+          <h1>Welcome to My website</h1>
+          <p>This is Home page of my website💕.</p>
         </div>
       </div>
       <Footer />
