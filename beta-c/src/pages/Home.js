f7e2cb442b1d6153720b7b3c4df223b3867d13b2
Harshchauhan177 <harsh7617rajput@gmail.com>
2025-10-06 01:14:59 +0530

    useReducer hook Done

--- beta-c/src/pages/Home.js
+++ beta-c/src/pages/Home.js
@@ -1,14 +1,65 @@
-import React from "react";
-// import mainStyles from "../assets/css/Main.module.css";
+import React, { useReducer } from "react";
+import mainStyles from "../assets/css/Main.module.css";
 import Navbar from "../components/layouts/Navbar";
 import Footer from "../components/layouts/Footer";
-import HomeForm from "../components/forms/HomeForm";
+// import HomeForm from "../components/forms/HomeForm";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return 0;
+    default:
+      return state;
+  }
+};
 
 function Home() {
+  const [count, dispatch] = useReducer(reducer, 0);
+
   return (
     <>
       <Navbar />
-      <HomeForm />
+      {/* <HomeForm /> */}
+      <div style={{ minHight: "80vh" }}>
+        <div className={mainStyles.homePage}>
+          <p
+            style={{
+              border: "1.8px solid gray",
+              padding: "10px 15px",
+              borderRadius: "5px",
+            }}
+          >
+            {count}
+          </p>
+          <button
+            onClick={() => {
+              dispatch({ type: "increment" });
+            }}
+          >
+            Increment
+          </button>
+          <br />
+          <button
+            onClick={() => {
+              dispatch({ type: "decrement" });
+            }}
+          >
+            Decrement
+          </button>
+          <br />
+          <button
+            onClick={() => {
+              dispatch({ type: "reset" });
+            }}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
       <Footer />
     </>
   );
